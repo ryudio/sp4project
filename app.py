f5cdb8e674551da501df60fcb7961216ce07da21
ryudio <raina.yudio@gmail.com>
2024-04-28 16:35:16 -0700

    Optimize memory usage for a free Render plan

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ figb.add_trace(go.Bar(x=female_counts['Year'], y=female_counts['count'], name='F
 figb.update_layout(title='Perpetrator Sex by Year', xaxis=dict(title='Year'), yaxis=dict(title='Count'), barmode='group')
 
 st.plotly_chart(figb)
-
+del df1, df2, male_counts, female_counts
 
 df['Weapon'] = df['Weapon'].replace(['Rifle', 'Shotgun', 'Handgun'],'Firearm')
 df['Weapon'].unique()
@@ -28,6 +28,7 @@ df3 = df3.groupby('Year')['Weapon'].value_counts().reset_index()
 
 figl = px.line(df3, x='Year', y='count', color='Weapon', template='plotly_dark', title='Weapon Used in Homicides 1980-2014')
 st.plotly_chart(figl)
+del df3
 
 df4 = df.groupby('Year')['State'].value_counts().reset_index()
 df4 = df4.groupby('State')['count'].sum().reset_index()
@@ -36,12 +37,14 @@ figp = px.pie(df4, values='count', names='State', template='plotly_dark', title=
 figp.update_traces(textposition='inside')
 figp.update_layout(uniformtext_minsize=12, uniformtext_mode='hide')
 st.plotly_chart(figp)
+del df4
 
 df5 = df.drop(df[df['Perpetrator Race'] == 'Unknown'].index)
 df5 = df5.groupby('Year')['Perpetrator Race'].value_counts().reset_index()
 
 figs = px.scatter(df5, x='Year', y='count', color='Perpetrator Race', template='plotly_dark', title='Perpetrator Race')
 st.plotly_chart(figs)
+del df5
 
 df['Perpetrator Age'] = df['Perpetrator Age'].replace(' ',0)
 df['Perpetrator Age'] = pd.to_numeric(df['Perpetrator Age'], errors='coerce')
@@ -51,6 +54,7 @@ df6.drop(df6[df6['Perpetrator Age'] < 5].index, inplace=True)
 
 figh = px.histogram(df6, x='Perpetrator Age', title='Homicide by Firearm 1980-2014')
 st.plotly_chart(figh)
+del df6
 
 st.subheader("Crime Solved Percentage CA/TX/NY")
 
@@ -61,3 +65,11 @@ stdf = stdf['Crime Solved'].value_counts()
 
 figp2 = px.pie(stdf, values=stdf.values, color=stdf.index, template='plotly_dark')
 st.plotly_chart(figp2)
+del stdf
+
+import os, psutil
+def usage():
+     process = psutil.Process(os.getpid())
+     return process.memory_info()[0] / float(2 ** 20)
+
+st.text(usage())
